846a1c3fbca4c0f7d36a306b9a8ec0aeb6c86773
Aeldit <raphael.roger.92@gmail.com>
2024-02-19 12:42:26 +0100

    Use CyanLib 0.4.3

--- src/main/java/fr/aeldit/cyansh/config/CyanSHConfig.java
+++ src/main/java/fr/aeldit/cyansh/config/CyanSHConfig.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023  -  Made by Aeldit
+ * Copyright (c) 2023-2024  -  Made by Aeldit
  *
  *              GNU LESSER GENERAL PUBLIC LICENSE
  *                  Version 3, 29 June 2007
@@ -17,20 +17,19 @@
 
 package fr.aeldit.cyansh.config;
 
-import fr.aeldit.cyanlib.lib.config.CyanLibOptionsStorage;
+import fr.aeldit.cyanlib.lib.config.BooleanOption;
+import fr.aeldit.cyanlib.lib.config.IntegerOption;
 import fr.aeldit.cyanlib.lib.utils.RULES;
-
-import static fr.aeldit.cyansh.util.Utils.CYANSH_OPTIONS_STORAGE;
 
 public class CyanSHConfig
 {
-    public static final CyanLibOptionsStorage.BooleanOption ALLOW_HOMES = CYANSH_OPTIONS_STORAGE.new BooleanOption("allowHomes", true);
-    public static final CyanLibOptionsStorage.BooleanOption ALLOW_BYPASS = CYANSH_OPTIONS_STORAGE.new BooleanOption("allowByPass", false);
+    public static final BooleanOption ALLOW_HOMES = new BooleanOption("allowHomes", true);
+    public static final BooleanOption ALLOW_BYPASS = new BooleanOption("allowByPass", false);
 
-    public static final CyanLibOptionsStorage.IntegerOption MIN_OP_LVL_HOMES = CYANSH_OPTIONS_STORAGE.new IntegerOption("minOpLvlHomes", 4, RULES.OP_LEVELS);
-    public static final CyanLibOptionsStorage.IntegerOption MAX_HOMES = CYANSH_OPTIONS_STORAGE.new IntegerOption("maxHomes", 20, RULES.POSITIVE_VALUE);
-    public static final CyanLibOptionsStorage.IntegerOption MIN_OP_LVL_BYPASS = CYANSH_OPTIONS_STORAGE.new IntegerOption("minOpLvlBypass", 4, RULES.OP_LEVELS);
+    public static final IntegerOption MIN_OP_LVL_HOMES = new IntegerOption("minOpLvlHomes", 4, RULES.OP_LEVELS);
+    public static final IntegerOption MAX_HOMES = new IntegerOption("maxHomes", 20, RULES.POSITIVE_VALUE);
+    public static final IntegerOption MIN_OP_LVL_BYPASS = new IntegerOption("minOpLvlBypass", 4, RULES.OP_LEVELS);
 
     // CyanLib Required Options
-    public static final CyanLibOptionsStorage.BooleanOption USE_CUSTOM_TRANSLATIONS = CYANSH_OPTIONS_STORAGE.new BooleanOption("useCustomTranslations", false, RULES.LOAD_CUSTOM_TRANSLATIONS);
+    public static final BooleanOption USE_CUSTOM_TRANSLATIONS = new BooleanOption("useCustomTranslations", false, RULES.LOAD_CUSTOM_TRANSLATIONS);
 }
