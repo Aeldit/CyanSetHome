dd23e4c09edf187d795ded86aee9d6dd695c85c0
aeldit <raphael.roger.92@gmail.com>
2024-03-12 09:51:59 +0100

    Use CyanLib 0.4.4

--- src/main/java/fr/aeldit/cyansh/config/CyanSHConfig.java
+++ src/main/java/fr/aeldit/cyansh/config/CyanSHConfig.java
@@ -29,7 +29,4 @@ public class CyanSHConfig
     public static final IntegerOption MIN_OP_LVL_HOMES = new IntegerOption("minOpLvlHomes", 4, RULES.OP_LEVELS);
     public static final IntegerOption MAX_HOMES = new IntegerOption("maxHomes", 20, RULES.POSITIVE_VALUE);
     public static final IntegerOption MIN_OP_LVL_BYPASS = new IntegerOption("minOpLvlBypass", 4, RULES.OP_LEVELS);
-
-    // CyanLib Required Options
-    public static final BooleanOption USE_CUSTOM_TRANSLATIONS = new BooleanOption("useCustomTranslations", false, RULES.LOAD_CUSTOM_TRANSLATIONS);
 }
